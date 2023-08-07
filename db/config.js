6d2983cf4a9844243698cb7ef04f0b40b5e05564
ENRIQUE ACOSTA RIVERA <enriqueacostarivera@comunidad.unam.mx>
2023-08-07 16:10:21 -0600

    fixed sequelize-cli bugs

--- db/config.js
+++ db/config.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 
-const USER = encodeURIComponent(process.env.DB_USER);
-const PASS = encodeURIComponent(process.env.DB_PASSWORD);
-const URI = `postgres://${USER}:${PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+const URI = process.env.INTERNAL_URL;
 
 module.exports = {
   development: {
