35b5b973981361ef98d211cdce3a0e129fa3faeb
ENRIQUE ACOSTA RIVERA <enriqueacostarivera@comunidad.unam.mx>
2023-08-07 15:58:01 -0600

    internal db url added

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -2,9 +2,8 @@ const { Sequelize } = require('sequelize');
 const setupModels = require('../db/models');
 require('dotenv').config();
 
-const USER = encodeURIComponent(process.env.DB_USER);
-const PASS = encodeURIComponent(process.env.DB_PASSWORD);
-const URI = `postgres://${USER}:${PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
+const URI = process.env.INTERNAL_URL;
 
 const sequelize = new Sequelize(URI, {
   dialect: 'postgres',
