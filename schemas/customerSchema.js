770cf739eea62098e9d37cb88c3b2ccf021055cb
ENRIQUE ACOSTA RIVERA <enriqueacostarivera@comunidad.unam.mx>
2023-08-17 01:29:35 -0600

    the customer schema was modified

--- schemas/customerSchema.js
+++ schemas/customerSchema.js
@@ -1,24 +1,24 @@
 const Joi = require('joi');
 
 const id = Joi.number().integer();
-const first_name = Joi.string().alphanum().min(3).max(32);
-const last_name = Joi.string().alphanum().min(3).max(32);
+const firstName = Joi.string().alphanum().min(3).max(32);
+const lastName = Joi.string().alphanum().min(3).max(32);
 const email = Joi.string().email().max(50);
 const password = Joi.string().min(6);
 const role = Joi.string().max(10);
 
 
 const createCustomerSchema = Joi.object({
-  first_name : first_name.required(),
-  last_name: last_name.required(),
+  firstName : firstName.required(),
+  lastName: lastName.required(),
   email: email.required(),
   password: password.required(),
   role: role
 });
 
 const updateCustomerSchema = Joi.object({
-  first_name : first_name,
-  last_name: last_name,
+  firstName : firstName,
+  lastName: lastName,
   email: email,
   password: password,
   role: role
